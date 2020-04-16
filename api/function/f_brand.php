fe3ec3c49b4f9ae5dfaacf4248452ee85946e27d
zaid1982 <hemppok.kembong@gmail.com>
2020-04-16 13:31:46 +0800

    Update section_item_counter display

---
+++ api/function/f_brand.php
@@ -0,0 +1,108 @@
+<?php
+
+class Class_brand
+{
+
+    private $constant;
+    private $fn_general;
+
+    function __construct()
+    {
+    }
+
+    private function get_exception($codes, $function, $line, $msg)
+    {
+        if ($msg != '') {
+            $pos = strpos($msg, '-');
+            if ($pos !== false) {
+                $msg = substr($msg, $pos + 2);
+            }
+            return "(ErrCode:" . $codes . ") [" . __CLASS__ . ":" . $function . ":" . $line . "] - " . $msg;
+        } else {
+            return "(ErrCode:" . $codes . ") [" . __CLASS__ . ":" . $function . ":" . $line . "]";
+        }
+    }
+
+    /**
+     * @param $property
+     * @return mixed
+     * @throws Exception
+     */
+    public function __get($property)
+    {
+        if (property_exists($this, $property)) {
+            return $this->$property;
+        } else {
+            throw new Exception($this->get_exception('0001', __FUNCTION__, __LINE__, 'Get Property not exist [' . $property . ']'));
+        }
+    }
+
+    /**
+     * @param $property
+     * @param $value
+     * @throws Exception
+     */
+    public function __set($property, $value)
+    {
+        if (property_exists($this, $property)) {
+            $this->$property = $value;
+        } else {
+            throw new Exception($this->get_exception('0002', __FUNCTION__, __LINE__, 'Get Property not exist [' . $property . ']'));
+        }
+    }
+
+    /**
+     * @param $property
+     * @return bool
+     * @throws Exception
+     */
+    public function __isset($property)
+    {
+        if (property_exists($this, $property)) {
+            return isset($this->$property);
+        } else {
+            throw new Exception($this->get_exception('0003', __FUNCTION__, __LINE__, 'Get Property not exist [' . $property . ']'));
+        }
+    }
+
+    /**
+     * @param $property
+     * @throws Exception
+     */
+    public function __unset($property)
+    {
+        if (property_exists($this, $property)) {
+            unset($this->$property);
+        } else {
+            throw new Exception($this->get_exception('0004', __FUNCTION__, __LINE__, 'Get Property not exist [' . $property . ']'));
+        }
+    }
+
+    /**
+     * @return array
+     * @throws Exception
+     */
+    public function get_brand_list()
+    {
+        try {
+            $this->fn_general->log_debug(__CLASS__, __FUNCTION__, __LINE__, 'Entering ' . __FUNCTION__);
+
+            $result = array();
+            $arr_dataLocal = Class_db::getInstance()->db_select('vm_brand');
+            foreach ($arr_dataLocal as $dataLocal) {
+                $row_result['brandId'] = $dataLocal['brand_id'];
+                $row_result['brandName'] = $dataLocal['brand_name'];
+                $row_result['manufacturerId'] = $dataLocal['manufacturer_id'];
+                $row_result['brandCostCarton'] = $this->fn_general->clear_null($dataLocal['brand_cost_carton']);
+                $row_result['brandCostUnit'] = $this->fn_general->clear_null($dataLocal['brand_cost_unit']);
+                array_push($result, $row_result);
+            }
+
+            return $result;
+        } catch (Exception $ex) {
+            $this->fn_general->log_error(__CLASS__, __FUNCTION__, __LINE__, $ex->getMessage());
+            throw new Exception($this->get_exception('0005', __FUNCTION__, __LINE__, $ex->getMessage()), $ex->getCode());
+        }
+    }
+
+}
--- js/pages/section_item_counter.js
+++ js/pages/section_item_counter.js
@@ -2,12 +2,98 @@ function SectionItemCounter() {
 
     const className = 'SectionItemCounter';
     let self = this;
+    let refBrand;
 
     this.init = function () {
-
+        self.generateItemCards();
     };
 
     this.getClassName = function () {
         return className;
+    };
+
+    this.generateItemCards = function () {
+        $('#divItcItems').html('');
+        for (let i=0; i<36; i++) {
+            const slotNo = i+1;
+            let htmlStr = '<div class="col-md-4 col-lg-3">\n' +
+                '            <div class="card mb-4">\n' +
+                '                <div class="p-3">\n' +
+                '                    <!-- Title -->\n' +
+                '                    <h4 class="card-title font-weight-bold mb-2">Slot '+slotNo+'\n' +
+                '                        <a href="#!" class="text-black-50">\n' +
+                '                            <i class="fas fa-edit pr-2 float-right"></i>\n' +
+                '                        </a>\n' +
+                '                    </h4>\n' +
+                '                    <!-- Subtitle -->\n' +
+                '                    <p class="card-text"><i class="fas fa-info-circle pr-2"></i>Bottle Slot, Column 1:1</p>\n' +
+                '                </div>\n' +
+                '                <div class="view overlay">\n' +
+                '                    <img class="card-img-top rounded-0" src="img/brand/brand_10.jpg" style="max-height: 80px; object-fit: contain">\n' +
+                '                    <a href="#!">\n' +
+                '                        <div class="mask rgba-white-slight"></div>\n' +
+                '                    </a>\n' +
+                '                </div>\n' +
+                '                <div class="card-body pb-3">\n' +
+                '                    <form id="formItc'+i+'">\n' +
+                '                        <div class="row">\n' +
+                '                            <div class="col-md-12">\n' +
+                '                                <select id="optItcBrand'+i+'" class="mdb-select md-form colorful-select dropdown-info mt-0" searchable="Search here">\n' +
+                '                                </select>\n' +
+                '                                <label for="optItcBrand'+i+'">Brand</label>\n' +
+                '                            </div>\n' +
+                '                            <div class="col-6">\n' +
+                '                                <div class="md-form mt-0">\n' +
+                '                                    <input type="number" id="txtItcCost'+i+'" class="form-control" value="1.30" disabled>\n' +
+                '                                    <label for="txtItcCost'+i+'" id="lblItcCost'+i+'" class="active">Cost</label>\n' +
+                '                                    <p class="font-small text-danger" id="txtItcCost'+i+'Err"></p>\n' +
+                '                                </div>\n' +
+                '                            </div>\n' +
+                '                            <div class="col-6">\n' +
+                '                                <div class="md-form mt-0">\n' +
+                '                                    <input type="number" id="txtItcPrice'+i+'" class="form-control" value="2.30" disabled>\n' +
+                '                                    <label for="txtItcPrice'+i+'" id="lblItcPrice'+i+'" class="active">Price</label>\n' +
+                '                                    <p class="font-small text-danger" id="txtItcPrice'+i+'Err"></p>\n' +
+                '                                </div>\n' +
+                '                            </div>\n' +
+                '                            <div class="col-12">\n' +
+                '                                <div class="md-form mt-0">\n' +
+                '                                    <input type="number" id="txtItcInitialReading'+i+'" class="form-control" value="1320" disabled>\n' +
+                '                                    <label for="txtItcInitialReading'+i+'" id="lblItcInitialReading'+i+'" class="active">Initial Reading</label>\n' +
+                '                                    <p class="font-small text-danger" id="txtItcInitialReading'+i+'Err"></p>\n' +
+                '                                </div>\n' +
+                '                            </div>\n' +
+                '                            <div class="col-12">\n' +
+                '                                <div class="md-form mt-0">\n' +
+                '                                    <input type="number" id="txtItcCurrentReading'+i+'" class="form-control" value="1320">\n' +
+                '                                    <label for="txtItcCurrentReading'+i+'" id="lblItcCurrentReading'+i+'" class="active">Current Reading</label>\n' +
+                '                                    <p class="font-small text-danger" id="txtItcCurrentReading'+i+'Err"></p>\n' +
+                '                                </div>\n' +
+                '                            </div>\n' +
+                '                            <div class="col-12">\n' +
+                '                                <div class="md-form mt-0 mb-0">\n' +
+                '                                    <input type="text" id="txtItcTotalSold'+i+'" class="form-control" value="12" disabled>\n' +
+                '                                    <label for="txtItcTotalSold'+i+'" id="lblItcTotalSold'+i+'" class="active">Total Sold</label>\n' +
+                '                                    <p class="font-small text-danger" id="txtItcTotalSold'+i+'Err"></p>\n' +
+                '                                </div>\n' +
+                '                            </div>\n' +
+                '                        </div>\n' +
+                '<!--                        <button id="btnItcSave1" class="btn btn-info waves-effect float-right"><i class="far fa-save ml-1"></i>&nbsp;&nbsp;Save</button>-->\n' +
+                '                    </form>\n' +
+                '                </div>\n' +
+                '            </div>\n' +
+                '        </div>';
+            $('#divItcItems').append(htmlStr);
+
+            mzOption('optItcBrand'+i, refBrand, 'Select Brand', 'brandId', 'brandName', {}, 'required');
+        }
+    };
+
+    this.getClassName = function () {
+        return className;
+    };
+
+    this.setRefBrand = function (_refBrand) {
+        refBrand = _refBrand;
     };
 }
